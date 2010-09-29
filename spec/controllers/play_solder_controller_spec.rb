9d5817fd4d014d59b19b5ec97a1b325551b9da93
Brad Wilson <brad@lucky-dip.net>
2010-09-30 06:07:17 +1000

    leave controller stuff for another gem

--- spec/controllers/play_solder_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe PlaySolderController do
-
-  it "should use PlaySolderController" do
-    controller.should be_an_instance_of(PlaySolderControllerController)
-  end
-
-end
